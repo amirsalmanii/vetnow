ccff24c082c451ab0b8fba288ed980f1fc9e452b
amirSalmani <salmaniamir13@gmail.com>
2021-12-14 18:51:16 +0330

    fix like system

--- vetnow/product/models.py
+++ vetnow/product/models.py
@@ -52,7 +52,8 @@ class Product(models.Model):
     descreption = models.TextField()
     price = models.BigIntegerField(default=0)
     quantity = models.BigIntegerField(default=0)
-    like = models.BigIntegerField(default=0, null=True, blank=True)
+    ilked = models.BooleanField(default=False, null=True, blank=True)
+    like_count = models.BigIntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.name
