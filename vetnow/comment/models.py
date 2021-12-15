8548662adc3a7de806cc7615725590323170f590
amirSalmani <salmaniamir13@gmail.com>
2021-12-15 18:42:55 +0330

    make comment model

--- vetnow/comment/models.py
+++ vetnow/comment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from product.models import Product
 
-# Create your models here.
+User = get_user_model()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
+    body = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
