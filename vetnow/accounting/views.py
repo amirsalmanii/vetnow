1824cf8bb2fa89805c06bd6a4e1584dbb36ef323
amirSalmani <salmaniamir13@gmail.com>
2021-12-28 22:10:41 +0330

    fix accounting logic

--- vetnow/accounting/views.py
+++ vetnow/accounting/views.py
@@ -1,10 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from product.models import Category, Product
+from order.models import Order
 from django.db.models import Sum
 
 
 class ComputeGain(APIView):
+    """
+    this class 
+    get category by slug and find all product with this category in order then:
+    compute all company_price and amount and assing to total prices and use (total_price - total_comapny_price)
+    and send to front_end
+    """
+
     def get(self, request, slug):
         category = Category.objects.filter(slug=slug)
         if category:
@@ -12,9 +20,16 @@ class ComputeGain(APIView):
         else:
             return Response(status=404)
 
-        products_price = category.products.all().aggregate(Sum('price'))
-        products_company_price = category.products.all().aggregate(Sum('company_price'))
-        print(products_price)
-        gains_ = products_price['price__sum'] - products_company_price['company_price__sum']
-        return Response({"gains": gains_}, status=200)
-        
+        products = category.products.all()
+        orders = []
+        total_amount = 0
+        total_company_price = 0
+        for p in products:
+            orders += p.orders_p.filter(payment_status='p')
+        for order in orders:
+            total_amount += order.amount
+            total_company_price += (order.product.all().aggregate(Sum('company_price'))).get('company_price__sum')
+        print(total_company_price)
+        print(total_amount)
+        gain_ = total_amount - total_company_price
+        return Response({"gains": gain_}, status=200)
