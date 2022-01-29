6d28f82f4aa65780c93f7a5118a2ff00150ef7be
amirSalmani <salmaniamir13@gmail.com>
2022-01-29 19:28:19 +0330

    add permissions to views

--- vetnow/news/views.py
+++ vetnow/news/views.py
@@ -18,7 +18,8 @@ class NewsListView(ListAPIView):
 class NewsCreateView(CreateAPIView):
     queryset = News.objects.all()
     serializer_class = serializers.NewsCreateSerializer
-    # permission_classes = (IsAdminUser,)
+    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAdminUser,)
 
 
 class NewsDetailView(RetrieveAPIView):
@@ -31,14 +32,14 @@ class NewsUpdateView(UpdateAPIView):
     queryset = News.objects.all()
     serializer_class = serializers.NewsCreateSerializer
     lookup_field = 'slug'
-    # permission_classes = (IsAdminUser,)
+    permission_classes = (IsAdminUser,)
 
 
 class NewsDeleteView(DestroyAPIView):
     queryset = News.objects.all()
     serializer_class = serializers.NewsCreateSerializer
     lookup_field = 'slug'
-    # permission_classes = (IsAdminUser,)
+    permission_classes = (IsAdminUser,)
 
 
 class AdminUserListView(ListAPIView):
