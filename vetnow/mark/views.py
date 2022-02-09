f3f51454826eb5118ccf2edbf52b8fda51ab0403
amirSalmani <salmaniamir13@gmail.com>
2022-02-09 09:57:32 +0330

    add pagination to mrked products list

--- vetnow/mark/views.py
+++ vetnow/mark/views.py
@@ -4,10 +4,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Mark
 from . import serializers
+from product.views import MyPagination
+
 
 class ListMarkedProducts(ListAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = serializers.MarkProductsSerializer
+    pagination_class = MyPagination
+
     def get_queryset(self):
         marks = Mark.objects.filter(user=self.request.user)
         return marks
@@ -25,6 +29,5 @@ class MarkedProductDelete(APIView):
                 return Response(status=204)
         else:
             return Response({"detail": "Authentication credentials were not provided."}, status=401)
-
 
 # im use product detail view for detail marked products
