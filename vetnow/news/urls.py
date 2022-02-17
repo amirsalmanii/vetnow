72c7133c2d0b535a2b5f51b2c9b81dab1dba8153
amirSalmani <salmaniamir13@gmail.com>
2022-02-17 12:36:06 +0330

    better path

--- vetnow/news/urls.py
+++ vetnow/news/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('news/', views.NewsListView.as_view(), name='news_list'),
     path('news/create/', views.NewsCreateView.as_view(), name='news_create'),
-    path('news/detail/<str:slug>/', views.NewsDetailView.as_view(), name='news_detail'),
+    path('news/<str:slug>/', views.NewsDetailView.as_view(), name='news_detail'),
     path('news/update/<str:slug>/', views.NewsUpdateView.as_view(), name='news_update'),
     path('news/delete/<str:slug>/', views.NewsDeleteView.as_view(), name='news_delete'),
     path('admin/user/list/', views.AdminUserListView.as_view(), name='admins_list'),
