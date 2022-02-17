142e04a29003ed48c95a7ba520b7b88c22a0675a
amirSalmani <salmaniamir13@gmail.com>
2022-02-17 12:35:11 +0330

    add and remove to favs list view and url implemented

--- vetnow/mark/urls.py
+++ vetnow/mark/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('marked/', views.ListMarkedProducts.as_view(), name='marked_products'),
     path('marked/delete/<int:pk>/', views.MarkedProductDelete.as_view(), name='marked_delete'),
+    path('marked/added_or_delete/<int:pk>/', views.CreateOrRemoveMarkProduct.as_view(), name='create_mark'),
 ]
--- vetnow/mark/views.py
+++ vetnow/mark/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from .models import Mark
 from . import serializers
 from product.views import MyPagination
-
+from product.models import Product
 
 class ListMarkedProducts(ListAPIView):
     permission_classes = (IsAuthenticated,)
@@ -30,4 +30,23 @@ class MarkedProductDelete(APIView):
         else:
             return Response({"detail": "Authentication credentials were not provided."}, status=401)
 
-# im use product detail view for detail marked products
+
+class CreateOrRemoveMarkProduct(APIView):
+    """
+    Here, if the user selects a product for his favorites,
+    if it already exists, he will delete the relationship, and if there is no 
+    relationship, he will add it to his favorites list.
+    """
+    def get(self, request, pk):
+        if request.user.is_authenticated:
+            product = Product.objects.get(id=pk)
+            is_exists = Mark.objects.filter(user=request.user, product=product)
+            if is_exists:
+                marked = is_exists.first()
+                marked.delete()
+                return Response('delete product from favs', status=204)
+            else:
+                Mark.objects.create(user=request.user, product=product)
+                return Response('added to marks', status=201)
+        else:
+            return Response({"detail": "Authentication credentials were not provided."}, status=401)         
