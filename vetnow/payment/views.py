7f25a2cc13a075d2d1d54131d42a0c35fe05ba29
amir@gmail.com <you@example.com>
2022-03-25 17:24:58 +0000

    payments add new urls for wallet

--- vetnow/payment/views.py
+++ vetnow/payment/views.py
@@ -7,16 +7,16 @@ from django.http import HttpResponse, Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from . import serializers
-
+from accounts.models import User
 
 class GoToGateWay(APIView):
     def post(self, request):
         serializer = serializers.TotalPriceSerializer(data=request.data)
         if serializer.is_valid():
-            amount = serializer.validated_data['total_payment']
+            amount = serializer.validated_data['total_price']
             user_mobile_number = request.user
-        # TODO else
-
+        else:
+            return Response(serializer.errors, status=400)
         factory = bankfactories.BankFactory()
         try:
             bank = factory.auto_create() # or factory.create(bank_models.BankType.BMI) or set identifier
@@ -28,7 +28,6 @@ class GoToGateWay(APIView):
 
 
             bank_record = bank.ready()
-
 
             return bank.redirect_gateway()
         except AZBankGatewaysException as e:
@@ -36,29 +35,6 @@ class GoToGateWay(APIView):
             # TODO: redirect to failed page.
             raise e
     
-    def get(self, request, payment):
-        amount = payment
-        user_mobile_number = '09914342975'
-
-        factory = bankfactories.BankFactory()
-        try:
-            bank = factory.auto_create() # or factory.create(bank_models.BankType.BMI) or set identifier
-            bank.set_request(request)
-            bank.set_amount(amount)
-
-            bank.set_client_callback_url(reverse('call_back_gt'))
-            bank.set_mobile_number(user_mobile_number)
-
-
-            bank_record = bank.ready()
-
-
-            return bank.redirect_gateway()
-        except AZBankGatewaysException as e:
-            logging.critical(e)
-            # TODO: redirect to failed page.
-            raise e
-       
 
 class VerifyFromGateWay(APIView):
     def get(self, request):
@@ -95,3 +71,76 @@ class VerifyToSendCart(APIView):
                     return Response(True, status=200)
                 return Response(False, status=400)
         return Response(serializer.errors, status=400)
+
+
+# wallet payment
+
+class GoToGateWayWallet(APIView):
+    def post(self, request):
+        serializer = serializers.TotalPriceSerializer(data=request.data)
+        if serializer.is_valid():
+            amount = serializer.validated_data['total_price']
+            user_mobile_number = request.user
+        else:
+            return Response(serializer.errors, status=400)
+        factory = bankfactories.BankFactory()
+        try:
+            bank = factory.auto_create() # or factory.create(bank_models.BankType.BMI) or set identifier
+            bank.set_request(request)
+            bank.set_amount(amount)
+
+            bank.set_client_callback_url(reverse('call_back_gt_wallet', args=[request.user, amount]))
+            bank.set_mobile_number(user_mobile_number)
+
+
+            bank_record = bank.ready()
+
+            # return Response('yes')
+            return bank.redirect_gateway()
+        except AZBankGatewaysException as e:
+            logging.critical(e)
+            # TODO: redirect to failed page.
+            raise e
+
+
+class VerifyFromGateWayWallet(APIView):
+    def get(self, request, usr, amount):
+        try:
+            user = User.objects.get(username=usr) # who added to him wallet
+        except:
+            pass
+        tracking_code = request.GET.get(settings.TRACKING_CODE_QUERY_PARAM, None)
+        if not tracking_code:
+            return redirect(f'http://45.159.113.83:3010/payments/failed?tc={tracking_code}')
+
+        try:
+            bank_record = bank_models.Bank.objects.get(tracking_code=tracking_code)
+        except bank_models.Bank.DoesNotExist:
+            return redirect(f'http://45.159.113.83:3010/payments/failed?tc={tracking_code}')
+
+        if bank_record.is_success:
+            user.wallet += amount
+            user.save()
+            return redirect(f'http://45.159.113.83:3010/payments/walletSuccessful?tc={tracking_code}')
+
+
+        # وقتی در راه اشتباهی میشه و پول گم بشه در چهل و هشت ساعت برمیگرده یا انصراف زدن
+        return redirect(f'http://45.159.113.83:3010/payments/failed?tc={tracking_code}')
+
+
+class BuyingWithWallet(APIView):
+    def get(self, request):
+        return Response(request.user.wallet, status=200)
+
+
+class UpdateUserWalletAfterBuying(APIView):
+    def post(self, request):
+        serializer = serializers.TotalPriceSerializer(data=request.data)
+        if serializer.is_valid():
+            user = request.user
+            amount = serializer.validated_data['total_price']
+            user.wallet = amount
+            user.save()
+            return Response(status=200)
+        return Response(serializer.errors, status=400)
+            
