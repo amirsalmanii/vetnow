d0e2276ce8ba51f89c4a602d648c1701d6634fe4
amir@gmail.com <you@example.com>
2022-04-16 14:19:54 +0430

    change view

--- vetnow/discount/views.py
+++ vetnow/discount/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView, CreateAPIView, RetrieveAPIView, DestroyAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Discount
 from . import serializers
 from product.views import MyPagination
@@ -12,15 +13,43 @@ class DiscountListView(ListAPIView):
     pagination_class = MyPagination
 
 
-class DiscountCreateView(CreateAPIView):
-    queryset = Discount.objects.all()
-    serializer_class = serializers.DiscountCreateSerializer
-    pagination_class = MyPagination
+# class DiscountCreateView(CreateAPIView):
+#     queryset = Discount.objects.all()
+#     serializer_class = serializers.DiscountCreateSerializer
+#     pagination_class = MyPagination
 
 
-class DiscountUpdateView(RetrieveUpdateAPIView):
-    queryset = Discount.objects.all()
-    serializer_class = serializers.DiscountCreateSerializer
+class DiscountCreateView(APIView):
+    def post(self, request):
+        serializer = serializers.DiscountCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            print('yes')
+            data = serializer.validated_data
+            products = data['products']
+            percent = data['discount_percent']
+            for pr in products:
+                pr.price_after_discount = pr.price - (1 / 100) * percent * pr.price
+                pr.save()
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+
+
+class DiscountUpdateView(APIView):
+    def put(self, request, pk):
+        discount = Discount.objects.get(id=pk)
+        serializer = serializers.DiscountCreateSerializer(instance=discount, data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            products = data['products']
+            percent = data['discount_percent']
+            for pr in products:
+                pr.price_after_discount = pr.price - (1 / 100) * percent * pr.price
+                pr.save()
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
 
 
 class DiscountDetailView(RetrieveAPIView):
